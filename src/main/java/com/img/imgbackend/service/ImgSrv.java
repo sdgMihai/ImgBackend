8614c5ccf23968e9ed58acaccf2702454e7f2285
Mihai GHEOACE (94307) <mihai.gheoace@stud.acs.upb.ro>
2022-05-19 18:00:54 +0300

    image service and image utils added

--- src/main/java/com/img/imgbackend/service/ImgSrv.java
+++ src/main/java/com/img/imgbackend/service/ImgSrv.java
@@ -5,106 +5,43 @@ import com.img.imgbackend.utils.Image;
 import com.img.imgbackend.utils.ImageUtils;
 import com.img.imgbackend.utils.ThreadSpecificData;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.BiConsumer;
 
 @Service
 public class ImgSrv {
-    double param;
+    public static final BiConsumer<List<Filter>, ThreadSpecificData> applyFilter = (List<Filter> filters, ThreadSpecificData data) -> {
+        final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(ImgSrv.class);
+        ListIterator<Filter> filterIt = filters.listIterator();
+        while (filterIt.hasNext()) {
+            Filter filter = filterIt.next();
+            log.debug("applying " + filter.toString() + "filter");
+            filter.applyFilter(data.getImage(), data.getNewImage(), data.getPARALLELISM());
+
+            if (filterIt.hasNext()) {
+                ImageUtils.swap(data.getImage(), data.getNewImage(), data.getPARALLELISM());
+            }
+        }
+    };
     @Value("${NUM_THREADS}")
-    Integer NUM_THREADS;
+    Integer PARALLELISM;
 
-    public Image process(Image image, String[] filterNames, String[] filterParams) {
-        assert (NUM_THREADS == 4);
-        List<Thread> threads = new ArrayList<>(NUM_THREADS);
-        List<ThreadSpecificData> specificDataList = new ArrayList<>(NUM_THREADS);
-        CyclicBarrier barrier = new CyclicBarrier(NUM_THREADS);
-        Lock lock = new ReentrantLock();
+    @Async
+    public CompletableFuture<Image> process(Image image, String[] filterNames, String[] filterParams) {
+        return CompletableFuture.supplyAsync(() -> {
+            assert (PARALLELISM == 4);
+            Image newImage = new Image(image.width - 2, image.height - 2);
+            final List<Filter> filters = FilterService.getFilters(filterNames, filterParams);
 
-        Image newImage = new Image(image.width - 2, image.height - 2);
-
-        for (int i = 0; i < NUM_THREADS; i++)
-            specificDataList.add(new ThreadSpecificData(i
-                    , barrier
-                    , lock
-                    , image
-                    , newImage
-                    , filterNames.length
-                    , NUM_THREADS
-                    , filterNames));
-
-        for (int i = 0; i < NUM_THREADS; i++) {
-            threads.add(
-                    new SubImageFilter(
-                            FilterService.getFilters(filterNames
-                                    , filterParams
-                                    , specificDataList.get(i))
-                            , specificDataList.get(i)));
-            threads.get(i).start();
-        }
-
-        for (int i = 0; i < NUM_THREADS; i++) {
-            try {
-                threads.get(i).join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        return newImage;
-    }
-
-    public static class SubImageFilter extends Thread {
-        private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(SubImageFilter.class);
-        List<Filter> filters;
-        ThreadSpecificData data;
-        int start;
-        int stop;
-
-        public SubImageFilter(List<Filter> filters, ThreadSpecificData data) {
-            this.filters = filters;
-            this.data = data;
-        }
-
-        @Override
-        public void run() {
-            int slice = (data.getImage().height - 2) / data.getNUM_THREADS();
-            start = Math.max(1, data.getThread_id() * slice);
-            stop = (data.getThread_id() + 1) * slice;
-            if (data.getThread_id() + 1 == data.getNUM_THREADS()) {
-                stop = Math.max((data.getThread_id() + 1) * slice, data.getImage().height - 1);
-            }
-
-            ListIterator<Filter> filterIt = filters.listIterator();
-            while (filterIt.hasNext()) {
-                int i = filterIt.nextIndex();
-                Filter filter = filterIt.next();
-                if (data.getThread_id() == 0) {
-                    log.debug(String.format("applying %d filters", filters.size()));
-                }
-
-                try {
-                    filter.applyFilter(data.getImage(), data.getNewImage(), 1, data.getImage().matrix.length - 1);
-                } catch (BrokenBarrierException | InterruptedException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    data.getBarrier().await();
-                } catch (InterruptedException | BrokenBarrierException e) {
-                    e.printStackTrace();
-                }
-                log.debug(i + "pass in thread no " + data.getThread_id() + " the barrier\n");
-                if (filterIt.hasNext()) {
-                    ImageUtils.swap(data.getImage(), data.getNewImage(), start, stop);
-                }
-            }
-        }
+            ImgSrv.applyFilter
+                    .accept(filters, new ThreadSpecificData(PARALLELISM, image, newImage));
+            return newImage;
+        });
     }
 
 }
--- src/main/java/com/img/imgbackend/utils/ImageUtils.java
+++ src/main/java/com/img/imgbackend/utils/ImageUtils.java
@@ -1,5 +1,8 @@
 package com.img.imgbackend.utils;
 
+import org.springframework.data.util.Pair;
+
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class ImageUtils {
@@ -11,4 +14,24 @@ public class ImageUtils {
                 });
     }
 
+    public static void swap(Image in, Image out, int PARALLELISM) {
+        final Pair<Integer, Integer>[] range = getRange(PARALLELISM, in.height);
+        Arrays.stream(range)
+                .parallel()
+                .forEach(rangeIt -> swap(in, out, rangeIt.getFirst(), rangeIt.getSecond()));
+    }
+
+    public static Pair<Integer, Integer>[] getRange(int PARALLELISM, int imgH) {
+        Pair<Integer, Integer>[] res = new Pair[PARALLELISM];
+        for (int i = 0; i < PARALLELISM; i++) {
+            int slice = (imgH - 2) / PARALLELISM;
+            int start = Math.max(1, i * slice);
+            int stop = (i + 1) * slice;
+            if (i + 1 == PARALLELISM) {
+                stop = Math.max((i + 1) * slice, imgH - 1);
+            }
+            res[i] = Pair.of(start, stop);
+        }
+        return res;
+    }
 }
