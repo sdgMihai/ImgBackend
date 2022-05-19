47b24f4115eec4bed31c51c15970e713036ed96e
Mihai GHEOACE (94307) <mihai.gheoace@stud.acs.upb.ro>
2022-05-19 18:01:27 +0300

    new ThreadSpecificData

--- src/main/java/com/img/imgbackend/utils/ThreadSpecificData.java
+++ src/main/java/com/img/imgbackend/utils/ThreadSpecificData.java
@@ -3,18 +3,10 @@ package com.img.imgbackend.utils;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.locks.Lock;
-
 @Data
 @AllArgsConstructor
 public class ThreadSpecificData {
-    int thread_id;
-    CyclicBarrier barrier;
-    Lock lock;
+    final int PARALLELISM;
     Image image;
     Image newImage;
-    int nrFilters;
-    int NUM_THREADS;
-    String[] filters;
 }
--- src/test/java/com/img/imgbackend/ThreadSpecificDataTest.java
+++ src/test/java/com/img/imgbackend/ThreadSpecificDataTest.java
@@ -4,39 +4,21 @@ import com.img.imgbackend.utils.Image;
 import com.img.imgbackend.utils.ThreadSpecificData;
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ThreadSpecificDataTest {
     @Test
     public void testEncapsulation() {
-        int threadID = 0;
-        CyclicBarrier cyclicBarrier = new CyclicBarrier(1);
-        Lock lock  = new ReentrantLock();
         Image input = new Image(1, 1);
         Image output = new Image(1, 1);
-        int nrFilters = 1;
-        int NUM_THREADS = 1;
-        String[] filterList = new String[0];
-        ThreadSpecificData capsule = new ThreadSpecificData(threadID
-                , cyclicBarrier
-                , lock
+        int PARALLELISM = 1;
+        ThreadSpecificData capsule = new ThreadSpecificData(
+                PARALLELISM
                 , input
                 , output
-                , nrFilters
-                , NUM_THREADS
-                , filterList
         );
-        assertEquals (threadID, capsule.getThread_id());
-        assertEquals(cyclicBarrier, capsule.getBarrier());
-        assertEquals(lock, capsule.getLock());
         assertEquals(input, capsule.getImage());
         assertEquals(output, capsule.getNewImage());
-        assertEquals(nrFilters, capsule.getNrFilters());
-        assertEquals(NUM_THREADS, capsule.getNUM_THREADS());
-        assertEquals(filterList, capsule.getFilters());
+        assertEquals(PARALLELISM, capsule.getPARALLELISM());
     }
 }
