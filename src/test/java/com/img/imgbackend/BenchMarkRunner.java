2e354e896a433d55f72a581c35cfc1cd56bd691d
Mihai GHEOACE (94307) <mihai.gheoace@stud.acs.upb.ro>
2022-05-20 17:17:48 +0300

    jmh test

---
+++ src/test/java/com/img/imgbackend/BenchMarkRunner.java
@@ -0,0 +1,9 @@
+package com.img.imgbackend;
+
+import java.io.IOException;
+
+public class BenchMarkRunner {
+    public static void main(String[] args) throws IOException {
+        org.openjdk.jmh.Main.main(args);
+    }
+}
--- src/test/java/com/img/imgbackend/PerformanceTest.java
+++ src/test/java/com/img/imgbackend/PerformanceTest.java
@@ -1,15 +1,13 @@
 package com.img.imgbackend;
 
-import com.img.imgbackend.filter.Filters;
+import com.img.imgbackend.filter.Filter;
+import com.img.imgbackend.filter.FilterFactory;
 import com.img.imgbackend.repository.ImageFormatIO;
-import com.img.imgbackend.service.FilterService;
 import com.img.imgbackend.service.ImgSrv;
 import com.img.imgbackend.utils.Image;
 import com.img.imgbackend.utils.ThreadSpecificData;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.openjdk.jmh.annotations.*;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -20,72 +18,69 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.openjdk.jmh.annotations.Scope.Benchmark;
 
+@State(Benchmark)
 @ExtendWith(SpringExtension.class)
 @Import(
         value = {
                 ImageFormatIO.class
         }
 )
-@Slf4j
 public class PerformanceTest {
-    private static final int NUM_THREADS = 4;
-    @Autowired
-    private ImageFormatIO imageFormatIO;
+    private static final int PARALLELISM = 4;
+    private ImageFormatIO imageFormatIO = new ImageFormatIO();
+    Image input;
+    Image output;
+    Image result;
 
-    @RepeatedTest(2)
-    public void testCannyEdgeDetectionFilter() throws IOException {
+    @Setup(Level.Invocation)
+    public void init() throws IOException {
         File inputFile = new ClassPathResource("noise.png").getFile();
         byte[] image = Files.readAllBytes(inputFile.toPath());
         assert (image.length != 0);
         BufferedImage bufferedImage = ImageIO.read(new ByteArrayInputStream(image));
-        final Image input = imageFormatIO.bufferedToModelImage(bufferedImage);
-        final Image output = new Image(input.width - 2, input.height - 2);
+        input = imageFormatIO.bufferedToModelImage(bufferedImage);
+        assertNotNull(input);
+        output = new Image(input.width - 2, input.height - 2);
+        assertNotNull(output);
 
         File outputResult = new ClassPathResource("respnoise.png").getFile();
         byte[] resultBytes = Files.readAllBytes(outputResult.toPath());
-        final Image result = imageFormatIO.bufferedToModelImage(
+        result = imageFormatIO.bufferedToModelImage(
                 ImageIO.read(new ByteArrayInputStream(
                         resultBytes
                 ))
         );
+    }
 
-        List<Thread> tasks = new ArrayList<>(NUM_THREADS);
-        String[] filterNames = new String[]{Filters.CANNY_EDGE_DETECTION.toString()};
-        Lock lock = new ReentrantLock();
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    @Warmup(iterations = 2)
+    @Measurement(iterations = 4)
+    public void testCannyEdgeDetectionFilter()  {
 
-        final CyclicBarrier barrier = new CyclicBarrier(NUM_THREADS);
-        List<ThreadSpecificData> specificDataList = new ArrayList<>(NUM_THREADS);
-        for (int i = 0; i < NUM_THREADS; i++)
-            specificDataList.add(new ThreadSpecificData(i, barrier, lock, input, output, filterNames.length, NUM_THREADS, filterNames));
+        final CompletableFuture<Image> imageCompletableFuture = CompletableFuture.supplyAsync(() -> {
+            Image newImage = new Image(input.width - 2, output.height - 2);
+            final Filter filter = FilterFactory.filterCreate("canny-edge-detection");
 
-        ImgSrv imgSrv = new ImgSrv();
+            ImgSrv.applyFilter
+                    .accept(List.of(filter), new ThreadSpecificData(PARALLELISM, input, output));
+            return newImage;
+        });
 
-        for (int i = 0; i < NUM_THREADS; i++) {
-            tasks.add(
-                    new ImgSrv.SubImageFilter(
-                            FilterService.getFilters(filterNames
-                                    , null
-                                    , specificDataList.get(i))
-                            , specificDataList.get(i)));
-            tasks.get(i).start();
-        }
+        imageCompletableFuture.join();
+    }
 
-        for (int i = 0; i < NUM_THREADS; i++) {
-            try {
-                tasks.get(i).join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
+    @TearDown(Level.Invocation)
+    public void checkResult() {
         assertEquals(input.width, output.width);
         assertEquals(input.height, output.height);
         assertEquals(result, output);
