ed0d9edaf26acdfb053c3d9b5024a12e172064df
Mihai GHEOACE (94307) <mihai.gheoace@stud.acs.upb.ro>
2022-06-03 14:35:00 +0300

    change annotations on ImageRestControllerIntegrationTest

--- src/test/java/com/img/imgbackend/ImageRestControllerIntegrationTest.java
+++ src/test/java/com/img/imgbackend/ImageRestControllerIntegrationTest.java
@@ -1,63 +1,48 @@
 package com.img.imgbackend;
 
-import com.img.imgbackend.model.ImgBin;
-import com.img.imgbackend.repository.ImageRepository;
+import com.img.imgbackend.controller.WebConfig;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.bson.types.Binary;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.io.File;
 import java.nio.file.Files;
-import java.util.Optional;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@SpringJUnitWebConfig(WebConfig.class)
+@AutoConfigureWebMvc
+@ContextConfiguration(classes = WebConfig.class)
 @TestPropertySource(
-  locations = "classpath:application.properties")
+        locations = "classpath:application.properties")
 public class ImageRestControllerIntegrationTest {
 
-    @Autowired
-    private MockMvc mvc;
-
-    @MockBean
-    private ImageRepository repository;
-
     private static final Logger log = LogManager.getLogger(ImageRestControllerIntegrationTest.class);
-
-    @BeforeEach
-    public void insertImage() throws Exception{
-        File imageFile = new ClassPathResource("Efficiency.png").getFile();
-        log.debug(imageFile.toPath());
-        byte[] image = Files.readAllBytes(imageFile.toPath());
-        ImgBin imgBinOne = new ImgBin("1", new Binary(image));
-        Mockito.when(repository.findById("1"))
-                .thenReturn(Optional.of(imgBinOne));
-    }
+    @Autowired
+    private WebApplicationContext webAppContext;
 
     @Test
     public void upload() throws Exception {
         File imageFile = new ClassPathResource("Efficiency.png").getFile();
         log.debug(imageFile.toPath());
         byte[] imageBytes = Files.readAllBytes(imageFile.toPath());
-        final MockPart image = new MockPart("Efficiency.png", "Efficiency.png","image/gif", imageBytes);
+        final MockPart image = new MockPart("Efficiency.png", "Efficiency.png", "image/gif", imageBytes);
 
+        MockMvc mvc = MockMvcBuilders.webAppContextSetup(webAppContext).build();
         mvc.perform(
-                MockMvcRequestBuilders.multipart("/api/filter")
-                        .file(image)
-                        .param("filter", "black-white"))
+                        MockMvcRequestBuilders.multipart("/api/filter")
+                                .file(image)
+                                .param("filter", "black-white"))
                 .andExpect(status().isOk());
     }
 }
