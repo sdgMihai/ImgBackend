1e2c2cdd2ee1bb7dbc19ee314f9df1e5fb464a7f
Mihai GHEOACE (94307) <mihai.gheoace@stud.acs.upb.ro>
2022-06-09 06:35:41 +0300

    smooth rate limiting

--- src/main/java/com/img/imgbackend/controller/Controller.java
+++ src/main/java/com/img/imgbackend/controller/Controller.java
@@ -1,5 +1,6 @@
 package com.img.imgbackend.controller;
 
+import com.google.common.util.concurrent.RateLimiter;
 import com.img.imgbackend.repository.ImageFormatIO;
 import com.img.imgbackend.service.ImgSrv;
 import com.img.imgbackend.utils.Image;
@@ -18,6 +19,7 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @RestController
@@ -25,15 +27,18 @@ import java.util.Iterator;
 public class Controller {
     private final ImgSrv imgSrv;
     private final ImageFormatIO imageFormatIO;
+    private final RateLimiter r;
 
     @Autowired
     public Controller(ImgSrv imgSrv, ImageFormatIO imageFormatIO) {
         this.imgSrv = imgSrv;
         this.imageFormatIO = imageFormatIO;
+        this.r = RateLimiter.create(3, 3, TimeUnit.SECONDS);
     }
 
     @PostMapping(value = "/filter", produces = MediaType.IMAGE_PNG_VALUE)
     public ResponseEntity<byte[]> filterImage(MultipartHttpServletRequest request) throws IOException {
+        r.acquire();
         Iterator<String> itr = request.getFileNames();
         MultipartFile file;
 
@@ -62,7 +67,8 @@ public class Controller {
                 res = imgSrv.process(input, filterNames, filterParams);
             } catch (InterruptedException e) {
                 e.printStackTrace();
-                return ResponseEntity.internalServerError().build();
+                return ResponseEntity.internalServerError()
+                        .build();
             }
 
             final BufferedImage image1 = imageFormatIO.modelToBufferedImage(res);
@@ -70,7 +76,8 @@ public class Controller {
             return ResponseEntity.ok(bytes);
         }
 
-        return null;
+        return ResponseEntity.internalServerError()
+                .build();
     }
 
 }
