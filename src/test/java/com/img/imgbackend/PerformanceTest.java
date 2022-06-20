9339fffc38aa3f24c7464ae5b96d624fe57e875b
Mihai GHEOACE (94307) <mihai.gheoace@stud.acs.upb.ro>
2022-06-20 17:16:14 +0300

    PerformanceTest becomes jmh test

--- src/test/java/com/img/imgbackend/PerformanceTest.java
+++ src/test/java/com/img/imgbackend/PerformanceTest.java
@@ -3,10 +3,8 @@ package com.img.imgbackend;
 import com.img.imgbackend.filter.tasks.TaskFactory;
 import com.img.imgbackend.repository.ImageFormatIO;
 import com.img.imgbackend.utils.Image;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.openjdk.jmh.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.io.ClassPathResource;
@@ -21,24 +19,57 @@ import java.nio.file.Files;
 import java.util.List;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.openjdk.jmh.annotations.Scope.Benchmark;
 
+@State(Benchmark)
 @ExtendWith(SpringExtension.class)
 @Import(
         value = {
                 ImageFormatIO.class
         }
 )
-@Slf4j
-@Disabled
+//@Disabled
 public class PerformanceTest {
     @Autowired
-    private ImageFormatIO imageFormatIO;
+    private ImageFormatIO imageFormatIO = new ImageFormatIO();
+
+    private static final int PARALLELISM = 4;
 
     private static final int THRESHOLD = 1250;
 
-    @RepeatedTest(1)
+    Image input;
+    Image output;
+    Image result;
+
+    @Setup(Level.Invocation)
+    public void init() throws IOException {
+        File inputFile = new ClassPathResource("noise.png").getFile();
+        byte[] image = Files.readAllBytes(inputFile.toPath());
+        assert (image.length != 0);
+        BufferedImage bufferedImage = ImageIO.read(new ByteArrayInputStream(image));
+        input = imageFormatIO.bufferedToModelImage(bufferedImage);
+        assertNotNull(input);
+        output = new Image(input.width - 2, input.height - 2);
+        assertNotNull(output);
+
+        File outputResult = new ClassPathResource("respnoise.png").getFile();
+        byte[] resultBytes = Files.readAllBytes(outputResult.toPath());
+        result = imageFormatIO.bufferedToModelImage(
+                ImageIO.read(new ByteArrayInputStream(
+                        resultBytes
+                ))
+        );
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    @Warmup(iterations = 2)
+    @Measurement(iterations = 4)
     public void testCannyEdgeDetectionFilter() throws IOException {
         File inputFile = new ClassPathResource("noise.png").getFile();
         byte[] image = Files.readAllBytes(inputFile.toPath());
@@ -60,25 +91,13 @@ public class PerformanceTest {
 
         List<RecursiveAction> filters = TaskFactory.getFilters(filterName, null, input, output, THRESHOLD);
         filters.forEach(commonPool::invoke);
+    }
 
+    @TearDown(Level.Invocation)
+    public void checkResult() {
         assertEquals(input.width, output.width);
         assertEquals(input.height, output.height);
         assertEquals(result, output);
-
-        for (int i = 1; i < output.height - 1; i++) {
-            for (int j = 1; j < output.width - 1; j++) {
-                if(!output.matrix[i][j].equals(result.matrix[i][j]) ) {
-                    log.debug(String.format("diff at [%d][%d], out = %s, res = %s"
-                            , i
-                            , j
-                            , output.matrix[i][j]
-                            , result.matrix[i][j]));
-                    assertEquals(output.matrix[i][j], result.matrix[i][j]);
-                    return;
-                }
-            }
-
-        }
     }
 
 }
